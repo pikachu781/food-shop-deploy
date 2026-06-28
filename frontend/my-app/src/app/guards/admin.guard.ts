0753c3ea28c0566c1b8d6a41a28ec9c5de057d5b
pikachu781 <hacker9946167@gmail.com>
2026-06-28 20:05:25 +0530

    Enhance admin guard to check token and role

--- frontend/my-app/src/app/guards/admin.guard.ts
+++ frontend/my-app/src/app/guards/admin.guard.ts
@@ -1,12 +1,18 @@
-import { CanActivateFn } from '@angular/router';
+import { CanActivateFn, Router } from '@angular/router';
+import { inject } from '@angular/core';
 
 export const adminGuard: CanActivateFn = () => {
 
-  const isAdmin = localStorage.getItem('adminLoggedIn');
+  const router = inject(Router);
 
-  if (isAdmin === 'true') {
+  const token = localStorage.getItem('token');
+  const role = localStorage.getItem('role');
+
+  if (token && role === 'ADMIN') {
     return true;
   }
 
+  router.navigate(['/admin-login']);
   return false;
+
 };
